ba4e5f2dace65e8b672c2dc1448bef50bc818a06
Dorota Bjoorn <Dorota.Bjoorn@iths2022>
2023-03-25 16:46:27 +0100

    moved remove_outliers() to my_functions.py

---
+++ lab/my_functions.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+def remove_outliers(df, col):
+    """remove outliers according to Tukeys rule"""
+    Q25 = df[col].quantile(.25)
+    Q75 = df[col].quantile(.75)
+    IQR = Q75-Q25
+    lower_bound = Q25 - 1.5 * IQR
+    higher_bound = Q75 + 1.5 * IQR
+    mask = (df[col] > lower_bound) & (df[col] < higher_bound)
+    df = df.loc[mask]
+    return df
