1b0c32b05d0460862b8dda56b27bc6bc3467dd8d
Dorota Bjoorn <Dorota.Bjoorn@iths2022>
2023-03-29 12:56:40 +0200

    added function scatterplot_boxplot()

--- lab/my_functions.py
+++ lab/my_functions.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def remove_outliers(df, col):
@@ -12,3 +14,9 @@ def remove_outliers(df, col):
     mask = (df[col] > lower_bound) & (df[col] < higher_bound)
     df = df.loc[mask]
     return df
+
+def scatterplot_boxplot(df, column_name):
+    fig, ax = plt.subplots(1, 2, figsize = (8, 4))
+    sns.scatterplot(data=df, x=df.index, y= column_name, ax=ax[0]);
+    sns.boxplot(data=df, x= column_name, ax=ax[1]);
+    return fig
